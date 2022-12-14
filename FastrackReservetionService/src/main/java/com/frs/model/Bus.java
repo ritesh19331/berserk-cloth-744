07c7f0d4468f6225de52b102ef36e2e078f8aa6c
Rup1999 <rup.bhunia1999@gmail.com>
2022-12-14 12:29:20 +0530

    bean added

--- FastrackReservetionService/src/main/java/com/frs/model/Bus.java
+++ FastrackReservetionService/src/main/java/com/frs/model/Bus.java
@@ -5,6 +5,9 @@ import java.time.LocalTime;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.data.annotation.Id;
 
 import lombok.AllArgsConstructor;
@@ -20,14 +23,28 @@ public class Bus {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer busId;
+	@NotNull(message = "Provide Bus Name, Because It Must Needed")
 	private String busName;
+	@NotNull(message = "Provide Bus Driver Name, Because It Must Needed")
 	private String driverName;
+	@NotNull(message = "Provide Bus Type Like Ac, Non-Ac, Sleeper, Double Decar, Single Decar and etc.")
 	private String busType;
+	@NotNull(message = "Provide Bus Starting Point")
 	private String routeFrom;
+	@NotNull(message = "Provide Bus Ending Point")
 	private String routeTo;
+	
+	@NotNull(message = "Provide Bus Arrival Time")
 	private LocalTime arrivalTime;
+	@NotNull(message = "Provide Bus Departure Time")
 	private LocalTime departureTime;
+	@NotNull(message = "Provide Total Number Of Seats")
 	private Integer seats;
+	@NotNull(message = "Provide Avaiable Number Of Seats")
 	private Integer avaiableSeats;
 
+	@NotNull(message = "Provide Amount Of Per Seat")
+	private Integer pricePerSeat;
+	@ManyToOne
+	private Route route;
 }
