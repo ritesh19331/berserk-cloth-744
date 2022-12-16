84a2eb5ad8e2ec462f9ff4480db70359abe14b03
Biswarup Bhunia <103635049+biswarup-bhunia@users.noreply.github.com>
2022-12-16 17:55:19 +0530

    Update Route

--- FastrackReservetionService/src/main/java/com/fastrack/model/Route.java
+++ FastrackReservetionService/src/main/java/com/fastrack/model/Route.java
@@ -10,7 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 
-import org.springframework.data.annotation.Id;
+import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
