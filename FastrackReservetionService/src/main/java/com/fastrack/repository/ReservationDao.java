dea682f90c6c8eaecb4ef51ceb17c7b2e9cf98ca
abhishek11125 <103635309+abhishek11125@users.noreply.github.com>
2022-12-16 14:28:44 +0530

    done with some changes

--- FastrackReservetionService/src/main/java/com/fastrack/repository/ReservationDao.java
+++ FastrackReservetionService/src/main/java/com/fastrack/repository/ReservationDao.java
@@ -1,6 +1,7 @@
 package com.fastrack.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.fastrack.model.Reservation;
 
--- FastrackReservetionService/src/main/java/com/fastrack/service/ReservationServiceDao.java
+++ FastrackReservetionService/src/main/java/com/fastrack/service/ReservationServiceDao.java
@@ -1,8 +1,11 @@
 package com.fastrack.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.fastrack.repository.ReservationDao;
+
+
 
 public class ReservationServiceDao implements ReservationService{
 	
